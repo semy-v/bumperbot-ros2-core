0bf5834f8773690c8f7dc2f60d2da158ac8abb5d
Sergii Myronov <myronov.sergii@gmail.com>
2026-06-26 15:05:26 -0400

    freertos setup

--- bumperbot_firmware/arduino/esp32_controller/src/main.cpp
+++ bumperbot_firmware/arduino/esp32_controller/src/main.cpp
@@ -34,275 +34,44 @@
  * the `Deactivate` message back to ROS 2.
  */
 
+#include <Arduino.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/queue.h>
+#include <freertos/task.h>
+
 #include "quadrature_encoder.hpp"
 #include "l298n_motor.hpp"
 #include "wheel_controller.hpp"
-#include "protocol/diff_drive_messages.hpp"
-#include "protocol/diff_drive_serialize.hpp"
-#include "protocol/diff_drive_deserialize.hpp"
+#include "task_shared_data.hpp"
+#include "serial_message_processor.hpp"
 
-// right wheel control pins
-#define L298N_EN_A 9
-#define L298N_IN_1 12
-#define L298N_IN_2 10
-#define RW_ENCODER_PHASE_A 3
-#define RW_ENCODER_PHASE_B 5
-
-// left wheel control pins
-#define L298N_EN_B 11
-#define L298N_IN_3 7
-#define L298N_IN_4 8
-#define LW_ENCODER_PHASE_A 2
-#define LW_ENCODER_PHASE_B 4
-
-// unconfigurable constants
-constexpr unsigned long kEmergencyStopTimeoutMs{2000};
-constexpr double kPulsePerRevolution{1280.0};
-
-// configurable constants (overriden by Config message)
-constexpr double kDefaultPidControlRate{25.0}; // Hz
-
-constexpr WheelConfig kDefaultRightMotorConfig{
-  .kp = 15.5,
-  .ki = 39.0,
-  .kd = 0.0,
-  .pwm_deadband = 17
-};
-
-constexpr WheelConfig kDefaultLeftMotorConfig{
-  .kp = 14.2,
-  .ki = 43.0,
-  .kd = 0.0,
-  .pwm_deadband = 18
-};
-
-// Initialize wheel controllers with defaults
-WheelController g_right_wheel{
-  L298NMotor{L298N_EN_A, L298N_IN_1, L298N_IN_2} /*motor*/,
-  QuadratureEncoder{RW_ENCODER_PHASE_A, RW_ENCODER_PHASE_B} /*encoder*/,
-  kDefaultPidControlRate /*pid_control_rate*/,
-  kDefaultRightMotorConfig /*wheel_config*/,
-  kPulsePerRevolution /*ticks_per_rev*/,
-  false /*invert_logic*/
-};
-
-WheelController g_left_wheel{
-  L298NMotor{L298N_EN_B, L298N_IN_3, L298N_IN_4} /*motor*/,
-  QuadratureEncoder{LW_ENCODER_PHASE_A, LW_ENCODER_PHASE_B} /*encoder*/,
-  kDefaultPidControlRate /*pid_control_rate*/,
-  kDefaultLeftMotorConfig /*wheel_config*/,
-  kPulsePerRevolution /*ticks_per_rev*/,
-  true /*invert_logic*/
-};
-
-// Serial input message process
-MessageStreamDeserializer<DiffDriveMessageRegistry> g_message_deserializer;
-bool g_received_velocity_msg{false};
-
-enum class InputProcessResult {
-  Success = 0,
-  MessageUnavailable = 1,
-  MessageInvalid = 2
-};
-
-constexpr MsgId AnyMsgId = MsgId::End;
-
-// Process next expected serial input message
-InputProcessResult processNextSerialInputMessage(const MsgId expected_msg_id = AnyMsgId);
-
-// Process all expected serial input messages
-InputProcessResult processAllSerialInputMessages(const MsgId expected_msg_id = AnyMsgId);
-
-// Serial output message process
-using DiffDriveMessageSerializer = MessageSerializer<DiffDriveMessageRegistry>;
-
-template<typename TData>
-void sendSerialMessage(const TData& message_data);
-
-template<MsgId Id>
-void sendSerialMessage();
-
-template<>
-void sendSerialMessage<MsgId::Velocity>();
-
-// Activate/Deactivate control
-unsigned long g_last_valid_msg_time_ms{};
-void processEmergencyDeactivation(const unsigned long current_time_ms = millis());
-void activate(const unsigned long current_time_ms = millis());
-void deactivate();
-
-void ARDUINO_ISR_ATTR rightWheelEncoderCallback();
-void ARDUINO_ISR_ATTR leftWheelEncoderCallback();
 
 void setup() {
   Serial.begin(115200);
   while(!Serial);
 
-  bool init_successful{false};
-
-  while(!init_successful) {
-    const auto init_msg_process_result = processAllSerialInputMessages(MsgId::Config);
-    if (InputProcessResult::Success != init_msg_process_result) {
-      // Wait until Init message received
-      continue;
-    }
-
-    g_right_wheel.begin();
-    g_left_wheel.begin();
-
-    // right wheel encoder interrupt setup
-    attachInterrupt(RW_ENCODER_PHASE_A, rightWheelEncoderCallback, CHANGE);
-    // left wheel encoder interrupt setup
-    attachInterrupt(LW_ENCODER_PHASE_A, leftWheelEncoderCallback, CHANGE);
-
-    init_successful = true;
-  }
-}
-
-void loop() {
-  const auto inputResult = processAllSerialInputMessages();
-
-  if (InputProcessResult::Success != inputResult) {
-    processEmergencyDeactivation();
-  }
-
-  g_right_wheel.update();
-  g_left_wheel.update();
-
-  if (InputProcessResult::MessageUnavailable != inputResult && g_received_velocity_msg) {
-    sendSerialMessage<MsgId::Velocity>();
-    g_received_velocity_msg = false;
-  }
-}
-
-InputProcessResult processNextSerialInputMessage(const MsgId expected_msg_id) {
-  InputProcessResult result{InputProcessResult::MessageUnavailable};
-
-  while(Serial.available()) {
-    const auto state = g_message_deserializer.processByte(Serial.read());
-
-    if (ProcessResult::SUCCESS == state) {
-      const MsgId next_msg_id = g_message_deserializer.getReceivedMessageId();
-      if (expected_msg_id != AnyMsgId && expected_msg_id != next_msg_id) {
-        // treat unexpected message as invalid
-        return InputProcessResult::MessageInvalid;
-      }
-
-      switch (next_msg_id) {
-        case MsgId::Config:
-        {
-          ConfigData config_data;
-          if (!g_message_deserializer.getPayload(config_data)) {
-            return InputProcessResult::MessageInvalid;
-          }
-
-          g_right_wheel.configure(config_data.pid_rate, config_data.r_wheel);
-          g_left_wheel.configure(config_data.pid_rate, config_data.l_wheel);
-
-          // Deactivate the wheels torque until next velocity command
-          deactivate();
-
-          // send same config data in response
-          sendSerialMessage(config_data);
-
-          return InputProcessResult::Success;
-        }
-        case MsgId::Velocity:
-        {
-          VelocityData vel_data;
-          if (!g_message_deserializer.getPayload(vel_data)) {
-            return InputProcessResult::MessageInvalid;
-          }
-
-          g_right_wheel.setTargetVelocity(vel_data.right_wheel_velocity);
-          g_left_wheel.setTargetVelocity(vel_data.left_wheel_velocity);
-
-          // keep wheels torque active for velocity command
-          activate();
-
-          // set flag to send velocity message response
-          g_received_velocity_msg = true;
-
-          return InputProcessResult::Success;
-        }
-        case MsgId::Deactivate:
-          // deactivate wheels torque
-          deactivate();
-          // send Deactivate message in response
-          sendSerialMessage<MsgId::Deactivate>();
-          return InputProcessResult::Success;
-
-        default:
-          return InputProcessResult::MessageInvalid;
-      }
-    } else if (ProcessResult::INCOMPLETE != state) {
-      // treat all error states as invalid message
-      return InputProcessResult::MessageInvalid;
-    }
-  }
-
-  return result;
-}
-
-InputProcessResult processAllSerialInputMessages(const MsgId expected_msg_id) {
-  auto next_msg_result = processNextSerialInputMessage(expected_msg_id);
-  bool valid_message_found = (InputProcessResult::Success == next_msg_result);
-
-  // MessageUnavailable is returned only if no serial input left
-  // meaning all possible input messages are processed
-  while(InputProcessResult::MessageUnavailable != next_msg_result) {
-    next_msg_result = processNextSerialInputMessage(expected_msg_id);
-    // accumulate valid message success result
-    valid_message_found |= (InputProcessResult::Success == next_msg_result);
-  }
-
-  return valid_message_found ? InputProcessResult::Success : next_msg_result;
-}
-
-template<typename TData>
-void sendSerialMessage(const TData& message_data) {
-  uint8_t message[DiffDriveMessageSerializer::getFrameSize<TData>()];
-  DiffDriveMessageSerializer::serialize(message_data, message);
-  Serial.write(message, sizeof(message));
-}
-
-template<MsgId TargetId>
-void sendSerialMessage() {
-  uint8_t message[DiffDriveMessageSerializer::template getFrameSize<TargetId>()];
-  DiffDriveMessageSerializer::template serialize<TargetId>(message);
-  Serial.write(message, sizeof(message));
-}
-
-template<>
-void sendSerialMessage<MsgId::Velocity>() {
-  const VelocityData data{
-    .right_wheel_velocity = g_right_wheel.getCurrentVelocity(),
-    .left_wheel_velocity = g_left_wheel.getCurrentVelocity()
+  static TaskSharedData task_shared_data{
+    .config_message_queue = xQueueCreate(1, sizeof(ConfigData)),
+    .current_velocity_queue = xQueueCreate(1, sizeof(VelocityData)),
+    .target_velocity_message_queue = xQueueCreate(1, sizeof(VelocityData)),
+    .diff_drive_control_task_handle = nullptr
   };
-  sendSerialMessage<VelocityData>(data);
-}
 
-void processEmergencyDeactivation(const unsigned long current_time_ms) {
-  if (current_time_ms - g_last_valid_msg_time_ms >= kEmergencyStopTimeoutMs) {
-    deactivate();
+  {
+    SerialInputProcessor input_processor(task_shared_data);
+    bool init_successful{false};
+    while(!init_successful) {
+      const auto result = input_processor.processNextSerialInputMessage(MsgId::Config);
+      init_successful = (SerialInputProcessor::Result::Success == result);
+    }
   }
+
+  // CPU core 0 tasks
+  xTaskCreatePinnedToCore(serialProcessTask, "SerialProcessTask", 4096, &task_shared_data, 2, NULL, 0);
+
+  // CPU core 1 tasks
+  xTaskCreatePinnedToCore(diffDriveControlTask, "DiffDriveControlTask", 4096, &task_shared_data, 2,
+    &task_shared_data.diff_drive_control_task_handle, 1);
 }
 
-void activate(const unsigned long current_time_ms) {
-  g_right_wheel.setActive();
-  g_left_wheel.setActive();
-  g_last_valid_msg_time_ms = current_time_ms;
-}
-
-void deactivate() {
-  g_right_wheel.setActive(false);
-  g_left_wheel.setActive(false);
-}
-
-void ARDUINO_ISR_ATTR rightWheelEncoderCallback() {
-  g_right_wheel.encoder().update();
-}
-
-void ARDUINO_ISR_ATTR leftWheelEncoderCallback() {
-  g_left_wheel.encoder().update();
-}
+void loop() {}
--- bumperbot_firmware/arduino/esp32_controller/src/wheel_controller.cpp
+++ bumperbot_firmware/arduino/esp32_controller/src/wheel_controller.cpp
@@ -23,27 +23,25 @@ void WheelController::configure(const double pid_control_rate,
 {
     reset();
 
+    pid_.SetTunings(wheel_config.kp, wheel_config.ki, wheel_config.kd);
+    pwm_deadband_ = static_cast<double>(wheel_config.pwm_deadband);
+
     // PID calculates PWM commands commands
     // forward (positive) and reverse (negative)
     pid_.SetOutputLimits(kMinPwm, kMaxPwm);
 
-    pid_.SetTunings(wheel_config.kp, wheel_config.ki, wheel_config.kd);
-
-    control_loop_period_ms_ = 1000.0 / pid_control_rate;
-
-    pwm_deadband_ = static_cast<double>(wheel_config.pwm_deadband);
-
     // Safety check to ensure 0 not passed to the PID library
-    const int sample_time = max(1.0, control_loop_period_ms_);
+    const int sample_time = max(1.0, 1000.0 / pid_control_rate);
     pid_.SetSampleTime(sample_time);
 }
 
 void WheelController::begin() {
     encoder_.begin();
     motor_.begin();
-    last_time_ms_ = millis();
     last_ticks_ = encoder_.read();
-    if (invert_logic_) last_ticks_ = -last_ticks_;
+    if (invert_logic_) {
+        last_ticks_ = -last_ticks_;
+    }
 }
 
 void WheelController::setActive(bool active) {
@@ -60,38 +58,27 @@ void WheelController::setActive(bool active) {
         pwm_cmd_ = 0.0;
         motor_.setPWM(0); 
 
-        // PID internal memory so it doesn't wind up while deactivated
+        // reset PID internal memory so
+        // it doesn't wind up while deactivated
         reset(); 
     }
 }
 
-void WheelController::update() {
-    const unsigned long current_time_ms = millis();
-    const unsigned long dt_ms = current_time_ms - last_time_ms_;
-
-    // Prevent unnecessary updates if called too rapidly
-    if (dt_ms < control_loop_period_ms_ || 0 == dt_ms) {
-        return;
-    }
-
+void WheelController::update(const double dt_sec) {
     long current_ticks = encoder_.read();
     if (invert_logic_) {
         current_ticks = -current_ticks;
     }
 
+    // kinematic calculation dt_sec
     const long delta_ticks = current_ticks - last_ticks_;
-
-    // kinematic calculation using actual dt
     const double delta_revs = static_cast<double>(delta_ticks) / ticks_per_rev_;
     const double delta_rads = delta_revs * 2.0 * PI;
-    const double dt_sec = static_cast<double>(dt_ms) / 1000.0;
-
     current_velocity_ = delta_rads / dt_sec;
 
     // Skip PID and motor updates while deactivated
     if (!is_active_) {
         last_ticks_ = current_ticks;
-        last_time_ms_ = current_time_ms;
         return; 
     }
 
@@ -130,5 +117,4 @@ void WheelController::update() {
 #endif // DEAD_BAND_CALIBRATION
 
     last_ticks_ = current_ticks;
-    last_time_ms_ = current_time_ms;
 }
