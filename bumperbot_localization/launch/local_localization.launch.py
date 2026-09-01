6375866eadd0a8014659505de6f9b21c19507b27
Sergii Myronov <myronov.sergii@gmail.com>
2026-09-01 15:32:54 -0400

    actualize controllers and localization config

--- bumperbot_localization/launch/local_localization.launch.py
+++ bumperbot_localization/launch/local_localization.launch.py
@@ -9,31 +9,6 @@ from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
 
-    static_transform_publisher = Node(
-        package="tf2_ros",
-        executable="static_transform_publisher",
-        arguments=[
-            "--x",
-            "0",
-            "--y",
-            "0",
-            "--z",
-            "0.103",
-            "--qx",
-            "1",
-            "--qy",
-            "0",
-            "--qz",
-            "0",
-            "--qw",
-            "0",
-            "--frame-id",
-            "base_footprint",
-            "--child-frame-id",
-            "imu_link",
-        ],
-    )
-
     robot_localization = Node(
         package="robot_localization",
         executable="ekf_node",
@@ -59,5 +34,8 @@ def generate_launch_description():
     )
 
     return LaunchDescription(
-        [static_transform_publisher, robot_localization, exit_handler]
+        [
+            robot_localization,
+            exit_handler,
+        ]
     )
